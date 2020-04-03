f8b2e9676a3fbddd47d2204b020b3276481a8b49
HUSEYNIYON <your>
2020-04-03 14:07:35 +0300

    created console.net core project

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace HomeWork
+{
+   class Rectangle
+   {
+       double side1, side2;
+       public Rectangle(double side1, double side2)
+       {
+           this.side1=side1;
+           this.side2=side2;
+
+       }
+       double AreaCalculator()
+       {
+           return side1*side2;
+       }
+       double PerimeterCalculator()
+       {
+           return (side1+side2)*2;
+       }
+       public double Area
+       {
+           get
+           {
+               return AreaCalculator();
+           }
+       }
+        public double Perimeter
+        {
+            get
+            {
+                return PerimeterCalculator();
+            }
+        }
+   }
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //Exercise_2
+            Console.WriteLine("Exercise_2");
+                        Console.Write("a=");
+            double a=Convert.ToDouble(Console.ReadLine());
+                        Console.Write("b=");
+            double b=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Rectangle rectangle=new Rectangle(a,b);
+            Console.WriteLine("Area="+rectangle.Area);
+            Console.WriteLine("Perimeter="+rectangle.Perimeter);
+            Console.ReadKey();
+
+        }
+
+    }
+}
